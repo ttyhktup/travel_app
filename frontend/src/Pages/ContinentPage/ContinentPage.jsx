b7b8f30fe7a3ba9be7bc7b712b5ab3cf5aaea5e3
Venera <zhargakovavenera@gmail.com>
2024-04-29 16:11:18 +0100

    continent page improved

--- frontend/src/Pages/ContinentPage/ContinentPage.jsx
+++ frontend/src/Pages/ContinentPage/ContinentPage.jsx
@@ -1,6 +1,7 @@
   import { useNavigate } from "react-router-dom";
   import "./ContinentPage.css"
   import { usePreferences } from "../../context/preferences";
+  import { useState } from "react";
 
 
   export const ContinentPage = () => {
@@ -11,52 +12,78 @@
         setPreferences({
             ...preferences,
             Continent: [...preferences.Continent, continent]
-        });
+        })
+    } else if (preferences.Continent.includes(continent)){
+      setPreferences({
+        ...preferences,
+      Continent: preferences.Continent.filter(item => item !== continent)
+      })
     }
-};
-        
+  };
+
     const navigate = useNavigate();
         const handleNextpage = () => {
         console.log(preferences)
         navigate('/date');
     }
+    
+    const [EuropeClass, setEuropeClass] = useState("continent-image-item")
+    const [AsiaClass, setAsiaClass] = useState("continent-image-item")
+    const [OceaniaClass, setOceaniaClass] = useState("continent-image-item")
+    const [NorthAmericaClass, setNorthAmericaClass] = useState("continent-image-item")
+    const [SouthAmericaClass, setSouthAmericaClass] = useState("continent-image-item")
+    const [AfricaClass, setAfricaClass] = useState("continent-image-item")
+    function addClassToEurope() {
+      setEuropeClass(prevClass => prevClass === "continent-image-item" ? "clicked" : "continent-image-item");
+    }
+    function addClassToAsia() {
+      setAsiaClass(prevClass => prevClass === "continent-image-item" ? "clicked" : "continent-image-item");
+    }
+    function addClassToOceania() {
+      setOceaniaClass(prevClass => prevClass === "continent-image-item" ? "clicked" : "continent-image-item");
+    }
+    function addClassToNorthAmerica() {
+      setNorthAmericaClass(prevClass => prevClass === "continent-image-item" ? "clicked" : "continent-image-item");
+    }
+    function addClassToSouthAmerica() {
+      setSouthAmericaClass(prevClass => prevClass === "continent-image-item" ? "clicked" : "continent-image-item");
+    }
+    function addClassToAfrica() {
+      setAfricaClass(prevClass => prevClass === "continent-image-item" ? "clicked" : "continent-image-item");
+    }
 
   return (
   <div className="continent-container">
-  <header>
-      <img src="/src/assets/plane.png" alt="" />
-      <h3>FuryRoam</h3>
-  </header>
   <div className="continent-body">
       <h4>1. Which continent would you like to visit?</h4>
+      <p>If you don't have a specific preference click next without selecting a continent.</p>
       <div className="continent-images">
           <div className="continent-items">
-            <img onClick={() => handleContinentSelect('Europe')} src="/src/assets/europe.png" alt="" />
+            <img className={EuropeClass} onClick={() => {handleContinentSelect('Europe'); addClassToEurope()}} src="/src/assets/europe.png" alt="" />
             <p>Europe</p>
           </div>
           <div className="continent-items">
-            <img onClick={() => handleContinentSelect('Asia')} src="/src/assets/asia.png" alt="" />
+            <img className={AsiaClass} onClick={() => {handleContinentSelect('Asia'); addClassToAsia()}} src="/src/assets/asia.png" alt="" />
             <p>Asia</p>
           </div>
           <div className="continent-items">
-            <img onClick={() => handleContinentSelect('Oceania')} src="/src/assets/oceania.png" alt="" />
+            <img className={OceaniaClass} onClick={() => {handleContinentSelect('Oceania'); addClassToOceania()}} src="/src/assets/oceania.png" alt="" />
             <p>Oceania</p>
           </div>
           <div className="continent-items">
-            <img onClick={() => handleContinentSelect('North America')} src="/src/assets/north-america.png" alt="" />
+            <img className={NorthAmericaClass} onClick={() => {handleContinentSelect('North America'); addClassToNorthAmerica()}} src="/src/assets/north-america.png" alt="" />
             <p>North America</p>
           </div>
           <div className="continent-items">
-            <img onClick={() => handleContinentSelect('South America')} src="/src/assets/south-america.png" alt="" />
+            <img className={SouthAmericaClass} onClick={() => {handleContinentSelect('South America'); addClassToSouthAmerica()}} src="/src/assets/south-america.png" alt="" />
             <p>South America</p>
           </div>
           <div className="continent-items">
-            <img onClick={() => handleContinentSelect('Africa')} src="/src/assets/africa.png" alt="" />
+            <img className={AfricaClass} onClick={() => {handleContinentSelect('Africa'); addClassToAfrica()}} src="/src/assets/africa.png" alt="" />
             <p>Africa</p>
           </div>
-          
       </div>
-      <button onClick={handleNextpage}>Next step</button>
+      <button className="continent-button" onClick={handleNextpage}>Next step</button>
   </div>
 
   </div>
