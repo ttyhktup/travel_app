6c69b557457fe77cb1b915b7576322f10ab664c3
Venera <zhargakovavenera@gmail.com>
2024-04-29 17:35:27 +0100

    weatherPage css doneeeee

--- frontend/src/Pages/WeatherPage/WeatherPage.jsx
+++ frontend/src/Pages/WeatherPage/WeatherPage.jsx
@@ -1,45 +1,63 @@
 import { useNavigate } from "react-router-dom";
 import { usePreferences } from "../../context/preferences";
 import { sendTravelPreferences } from "../../Services/BackendService";
-
+import "./WeatherPage.css"
 
 export const WeatherPage = () => {
-    const { preferences, setPreferences } = usePreferences();
-    const handleWeatherSelect = (mintemp, maxtemp) => {
+const { preferences, setPreferences } = usePreferences();
+const handleWeatherSelect = (mintemp, maxtemp) => {
 
-    if (!preferences.MinTemp.includes(mintemp)) {
-        setPreferences((prevPreferences) => ({
-            ...prevPreferences,
-            MinTemp: [...prevPreferences.MinTemp, mintemp]
-        }));
-    }
+if (!preferences.MinTemp.includes(mintemp)) {
+    setPreferences((prevPreferences) => ({
+        ...prevPreferences,
+        MinTemp: [...prevPreferences.MinTemp, mintemp]
+    }));
+}
 
-    // Check if the max temp is already selected; if not, add it to the array
-    if (!preferences.MaxTemp.includes(maxtemp)) {
-        setPreferences((prevPreferences) => ({
-            ...prevPreferences,
-            MaxTemp: [...prevPreferences.MaxTemp, maxtemp]
-        }));
-    }
+// Check if the max temp is already selected; if not, add it to the array
+if (!preferences.MaxTemp.includes(maxtemp)) {
+    setPreferences((prevPreferences) => ({
+        ...prevPreferences,
+        MaxTemp: [...prevPreferences.MaxTemp, maxtemp]
+    }));
+}
 };
-    
-    const navigate = useNavigate();
-    const handleNextpage = () => {
-        console.log(preferences)
-        sendTravelPreferences(preferences);
-        navigate('/Recommendations')
-    }
 
-    return (
-        <div className="Weather">
-            <h3>3. What weather would you prefer?</h3>
-            <button onClick={() => handleWeatherSelect(30, 1000)}>I prefer hot weather (above 30°C)</button>
-            <button onClick={() => handleWeatherSelect(20, 29)}>I prefer warm weather and sunshine (20°C - 29°C)</button>
-            <button onClick={() => handleWeatherSelect(15, 19)}>I enjoy mild temperatures with a mix of sun and clouds (15°C - 19°C)</button>
-            <button onClick={() => handleWeatherSelect(5, 14)}>I like cooler weather but not too cold (5°C - 14°C)</button>
-            <button onClick={() => handleWeatherSelect(5, -1000)}>I prefer cold weather with snow and winter activities (below 5°C)</button>
-            <button onClick={() => handleWeatherSelect(-1000, 1000)}>I have no strong preference for weather.</button>
-            <button onClick={handleNextpage}>Generate Result</button>
-        </div>
-    )
+const navigate = useNavigate();
+const handleNextpage = () => {
+    console.log(preferences)
+    sendTravelPreferences(preferences);
+    navigate('/Recommendations')
+}
+
+return (
+    <div className="weather-container">
+        <h4>3. What weather would you prefer?</h4>
+        <label className="checkbox-container" onClick={() => handleWeatherSelect(30, 1000)}>
+            <input type="radio" name="option"/>
+            I prefer hot weather (above 30°C)
+        </label>
+        <label className="checkbox-container" onClick={() => handleWeatherSelect(20, 29)}>
+            <input type="radio" name="option"/>
+            I prefer warm weather and sunshine (20°C - 29°C)
+        </label>
+        <label className="checkbox-container" onClick={() => handleWeatherSelect(15, 19)}>
+            <input type="radio" name="option"/>
+            I enjoy mild temperatures with a mix of sun and clouds (15°C - 19°C)
+        </label>
+        <label className="checkbox-container" onClick={() => handleWeatherSelect(5, 14)}>
+            <input type="radio" name="option"/>
+            I like cooler weather but not too cold (5°C - 14°C)
+        </label>
+        <label className="checkbox-container" onClick={() => handleWeatherSelect(5, -1000)}>
+            <input type="radio" name="option"/>
+            I prefer cold weather with snow and winter activities (below 5°C)
+        </label>
+        <label className="checkbox-container" onClick={() => handleWeatherSelect(-1000, -1000)}>
+            <input type="radio" name="option"/>
+            I have no strong preference for weather.
+        </label>
+        <button className="weather-button" onClick={handleNextpage}>Generate Result</button>
+    </div>
+)
 }
