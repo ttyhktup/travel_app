df6510737723865ecd5de94eabd9315007e2b429
George Paul <george.paul5236@gmail.com>
2024-04-29 18:06:43 +0100

    added test for get_hotel_link function

--- api/tests/test_location.py
+++ api/tests/test_location.py
@@ -31,4 +31,19 @@ def test_get_weather(mocker):
 
         assert test_location.city_weather["city1"] == ["fake_country", 25, test_location.get_hotel_link("city1", "2023-07-13", "2023-07-15")]
 
-
+        mocked_get.assert_called_once_with(
+            "https://archive-api.open-meteo.com/v1/archive",
+            params={
+                "latitude": 34.05,
+                "longitude": -118.25,
+                "start_date": "2023-07-13",
+                "end_date": "2023-07-15",
+                "hourly": "temperature_2m",
+                "timezone": "GMT"
+            }
+        )
+def test_get_hotel_link(mocker): 
+    mocker.patch('lib.Location.get_cities', return_value="this is mocked")
+    test_location = Location("123", "fake_country")
+    link = test_location.get_hotel_link("city1", "2023-07-13", "2023-07-15")
+    assert link == "https://www.booking.com/searchresults.en-gb.html?ss=city1&checkin=2024-07-13&checkout=2024-07-15&group_adults=2&no_rooms=1&group_children=0"
